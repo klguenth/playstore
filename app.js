89ddfe55e30c3b55df4fdf1415e66664bf6fea77
klguenth <klguenth@gmail.com>
2019-09-30 20:06:15 -0500

    updated

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 app.use(morgan('common'));
 
-    const apps = require('./data.js');
+const apps = require('./data.js');
 
 
 app.get('/apps', (req, res) => {
-    const { search = "", sort, genre } = req.query;
+    const { search = "", sort, genres } = req.query;
     if (sort) {
         if (!['rating', 'app'].includes(sort)) {
             return res
@@ -17,24 +18,22 @@ app.get('/apps', (req, res) => {
                 .send('Sort must be one of rating or app');
         }
     }
-    if (genre) {
-        if (!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genre)) {
+    if (genres) {
+        if (!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genres)) {
             return res
                 .status(400)
                 .send('Genre must be one of the following: Action, Puzzle, Strategy, Casual, Arcade, Card');
         }
     }
 
-    if (res === (search, sort, genre)) {
-        return data;
+    if (res === (search, sort, genres)) {
+        return res;
     }
 
     let results = apps
         .filter(app =>
-            app
-                .title
-                .toLowerCase()
-                .includes(search.toLowerCase()));
+            app);
+
   res
     .json(results);
 });
